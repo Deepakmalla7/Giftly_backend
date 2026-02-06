736ec0ad6623ff2895d041d5b2ee4bb5b27c4751
Deepakmalla7 <malladeepakj@gmail.com>
2026-02-06 10:05:00 +0545

    refactor(user): align dto and type for user payload

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,35 +1,55 @@
-import { z } from "zod";
-import { UserSchema } from "../types/User.type";
+import z from "zod";
+import { UserSchema } from "../types/user.type";
 
-export const createUserDto = UserSchema.pick({
-  fname: true,
-  lname: true,
-  email: true,
-  password: true,
-  phoneNumber: true,
-}).extend({
-  confirmPassword: z.string().min(6, "Confirm password is required"),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"],
-});
+export const createUserDto = UserSchema.pick(
+    {
+        firstName: true,
+        lastName:true,
+        username:true,
+        email:true,
+        password:true
+    }
+).extend(
+    {
+        confirmPassword:z.string().min(3)
+    }
+).refine(
+    (data) => data.password === data.confirmPassword,
+    {
+        message:"Password do not match",
+        path:["confirmPassword"]
+    }
+)
 
-export const updateUserDto = UserSchema.partial().extend({
-  confirmPassword: z.string().min(6).optional(),
-}).refine((data) => {
-  if (data.password || data.confirmPassword) {
-    return data.password === data.confirmPassword;
-  }
-  return true;
-}, {
-  message: "Passwords do not match",
-});
+export type createUserDto = z.infer<typeof createUserDto>;
 
-export const loginUserDto = z.object({
-  email: z.string().min(1, "Email is required").email("Invalid email format"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-});
 
-export type UpdateUserDTO = z.infer<typeof updateUserDto>;
-export type CreateUserDTO = z.infer<typeof createUserDto>;
-export type LoginUserDTO = z.infer<typeof loginUserDto>;
+export const updateUserDto = UserSchema.partial().extend(
+    {
+        confirmPassword:z.string().min(3).optional()
+    }
+).refine(
+    (data) => {
+        if(data.password || data.confirmPassword){
+            return data.password === data.confirmPassword;
+        }
+        return true;
+    },
+    {
+        message:"Password do not match",
+    }
+)
+
+export type updateUserDto = z.infer<typeof updateUserDto>;
+
+
+//if anything is sent from client then we need to have a dto to validate that data
+
+export const loginUserDto = z.object(
+    {
+        email:z.string().min(1,"Email is required").email("Invalid email format"),
+        password:z.string().min(3,"Password must be at least 3 characters long")
+    }
+)
+
+export type loginUserDto = z.infer<typeof loginUserDto>;
